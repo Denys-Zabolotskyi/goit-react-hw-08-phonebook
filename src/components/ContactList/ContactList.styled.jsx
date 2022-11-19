492e2e9b9fab1be613613fa188fb32a87a736b89
Denys Zabolotskyi <d.zabolotskyi@gmail.com>
2022-11-20 01:49:11 +0200

    bug fixed (delete button)

--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -3,24 +3,25 @@ import styled from '@emotion/styled';
 export const List = styled.ul`
   padding:10px;
   background-color: #cccccc;
-  border-radius: 10px;
 `;
 
 export const Item = styled.li`
-  justify-content: space-between;
   display: flex;
-  &:not(:last-child) {
-    align-items: baseline;
+  justify-content: space-between;
+  align-items: baseline;
+  // &:not(:last-child) {
+  //   align-items: baseline;
 }
 `;
 
 export const Button = styled.button`
+  height: 35px;
   padding:5px;
   width: 100px;
+  hig
   font-weight: 400;
   font-size: 15px;
   line-height: 1.25;
-  border: none;
   border-radius: 10px;
   border: 2px solid #fff;
   color: #fff;
